e32f43334505bc898f24ee7c18fd4aff8b9b5ac9
Anson <ansfoong@gmail.com>
2021-10-20 05:36:40 -0700

    Initial Commit

---
+++ src/main/java/io/serviceshark/api/authentication/AuthenticationController.java
@@ -0,0 +1,8 @@
+package io.serviceshark.api.authentication;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("AuthenticationController")
+public class AuthenticationController {
+  
+}
---
+++ src/test/java/io/serviceshark/api/ServicesharkSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.serviceshark.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesharkSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
